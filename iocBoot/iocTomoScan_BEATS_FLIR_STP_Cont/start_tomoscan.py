af7079a96403b2bf2e5e2045332029dde94c5b1c
Mustafa-zubi <mostafa.zoubi@sesame.org.jo>
2023-02-16 12:01:18 +0200

    delete the old setup -STP-

--- iocBoot/iocTomoScan_BEATS_FLIR_STP_Cont/start_tomoscan.py
+++
@@ -1,8 +0,0 @@
-# This script creates an object of type TomoScan2BM for doing tomography scans at APS beamline 2-BM-A
-# To run this script type the following:
-#     python -i start_tomoscan_2bm.py
-# The -i is needed to keep Python running, otherwise it will create the object and exit
-from tomoscan.tomoscan_BEATS_FLIR_STP_Cont import TomoScanBEATSFlirStpCont
-ts = TomoScanBEATSFlirStpCont(["../../db/tomoScan_settings.req",
-                  "../../db/tomoScan_BEATS_FLIR_STP_Cont_settings.req"], 
-                 {"$(P)":"tomoscanBEATS:", "$(R)":"FlirStpCont:"})
