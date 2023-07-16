8920f78533983f9b0a3acd8209279ae33ac5d2bf
Anas Mohammad <104130778+Anasmoh999@users.noreply.github.com>
2023-07-16 14:12:07 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -72,7 +72,7 @@ master_doc = 'index'
 # General information about the project.
 Affiliation = u'Argonne National Laboratory'
 project = u'BEATS User Manual'
-copyright = u'2020, ' + Affiliation
+copyright = u'2023, ' + Affiliation
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
